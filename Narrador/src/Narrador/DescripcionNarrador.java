53c92db0abe2598c6ec0b7091e0e21b4d63babec
Roku <24.damt.rodrigoacena@fundacionmontessori.com>
2024-11-19 20:38:29 +0100

    Mas cambios a Narrador

--- Narrador/src/Narrador/DescripcionNarrador.java
+++ Narrador/src/Narrador/DescripcionNarrador.java
@@ -3,15 +3,9 @@ package Narrador;
 public class DescripcionNarrador {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Nombre en clave: Nomada");
 		System.out.println("Tipo de narrador: Omnisciente");
 		System.out.println();
-
-=======
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
->>>>>>> 2024160e57355e40e9793f84005291bbaa3c00b0
 	}
 
 }
