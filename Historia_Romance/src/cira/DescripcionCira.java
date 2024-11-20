ece4657ceb38fac4b5054c324b0325178b8231e9
Roku <24.damt.rodrigoacena@fundacionmontessori.com>
2024-11-20 10:40:04 +0100

    Narrador cambiado a Historia-Romance

--- Historia_Romance/src/cira/DescripcionCira.java
+++ Historia_Romance/src/cira/DescripcionCira.java
@@ -1,4 +1,4 @@
-package Cira;
+package cira;
 
 public class DescripcionCira {
 
---
+++ Historia_Romance/src/narrador/DescripcionNarrador.java
@@ -0,0 +1,11 @@
+package narrador;
+
+public class DescripcionNarrador {
+
+	public static void main(String[] args) {
+		System.out.println("Nombre en clave: Nomada");
+		System.out.println("Tipo de narrador: Omnisciente");
+		System.out.println();
+	}
+
+}
