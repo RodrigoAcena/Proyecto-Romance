cf26fdfe98205276c9000d7bd925d9d14bf401a4
kellyenesencia <kellyvasquez1921@gmail.com>
2024-11-26 20:55:51 +0100

    Descripción Cira, segunda parte

--- Historia_Romance/src/cira/DescripcionCira.java
+++ Historia_Romance/src/cira/DescripcionCira.java
@@ -7,6 +7,17 @@ public class DescripcionCira {
 				+ "Inicialmente, es presentada como una joven pastora que anhela más de su vida, "
 				+ "soñando con la aventura y un destino más allá de su rutina diaria. "
 				+ "Es una mujer con un espíritu libre y curioso, que no se conforma con la vida que le ha tocado vivir.");
+		
+		System.out.println("Cira es un personaje multifacético que evoluciona a lo largo de la historia. "
+				+ "Inicialmente, es presentada como una joven pastora que anhela más de su vida, "
+				+ "soñando con la aventura y un destino más allá de su rutina diaria. Es una mujer con un espíritu libre y curioso, "
+				+ "que no se conforma con la vida que le ha tocado vivir."
+				+ "\r\n"
+				+ "\r\n"
+				+ "A medida que avanza la trama, Cira demuestra ser una persona valiente y decidida. "
+				+ "No teme enfrentar desafíos y se muestra dispuesta a luchar por lo que cree justo. "
+				+ "Su encuentro con el nomada y la obtención del tesoro la convierten en una figura central "
+				+ "en la lucha contra la codicia de Umbra.");
 	}
 
 }
