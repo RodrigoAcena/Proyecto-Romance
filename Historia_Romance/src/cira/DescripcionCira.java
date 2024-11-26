df4f7fe77deae12c00df9da93e4a4174c4334550
kellyenesencia <kellyvasquez1921@gmail.com>
2024-11-26 20:53:39 +0100

    Descripción Cira, primera parte

--- Historia_Romance/src/cira/DescripcionCira.java
+++ Historia_Romance/src/cira/DescripcionCira.java
@@ -3,7 +3,10 @@ package cira;
 public class DescripcionCira {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Cira es un personaje multifacético que evoluciona a lo largo de la historia. "
+				+ "Inicialmente, es presentada como una joven pastora que anhela más de su vida, "
+				+ "soñando con la aventura y un destino más allá de su rutina diaria. "
+				+ "Es una mujer con un espíritu libre y curioso, que no se conforma con la vida que le ha tocado vivir.");
 	}
 
 }
