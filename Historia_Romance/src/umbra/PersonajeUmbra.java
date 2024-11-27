bd6514aadc4e6fee9a57b7ef6186fea99bf7262e
Kelly Vasquez <kellyvasquez1921@gmail.com>
2024-11-27 17:28:19 +0100

    Desarrollo de personaje Umbra 1
    Desarrollo de personaje Umbra, primera parte.

--- Historia_Romance/src/umbra/PersonajeUmbra.java
+++ Historia_Romance/src/umbra/PersonajeUmbra.java
@@ -3,7 +3,16 @@ package umbra;
 public class PersonajeUmbra {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Umbra: El Antagonista Ambicioso\r\n"
+				+ "Umbra es presentado como el principal antagonista de esta historia, "
+				+ "un personaje motivado por la codicia y el deseo de poder absoluto. "
+				+ "Su personalidad se define por varios rasgos clave:\r\n"
+				+ "\r\n"
+				+ "Ambicioso: Su deseo de obtener el tesoro es incansable. Ve en él una forma de alcanzar la riqueza y el poder que cree merecer.\r\n"
+				+ "Manipulador: Umbra utiliza su posición de autoridad para controlar a sus súbditos y lograr sus objetivos.\r\n"
+				+ "Despiadado: No duda en recurrir a la violencia para eliminar a quienes se oponen a él, demostrando una falta total de empatía.\r\n"
+				+ "Inseguro: Detrás de su fachada de poder, Umbra oculta una profunda inseguridad. Su obsesión por el tesoro podría ser una forma de compensar esta carencia.\r\n"
+				+ "Orgulloso: Su orgullo lo ciega ante las consecuencias de sus acciones y lo lleva a subestimar a sus enemigos.");
 
 	}
 
