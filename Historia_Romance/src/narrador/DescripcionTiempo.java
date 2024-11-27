1a407b72007f8baea9e60c9430481edf6cc81168
Kelly Vasquez <kellyvasquez1921@gmail.com>
2024-11-27 17:24:54 +0100

    Creación personaje Umbra
    Creación de paquete y clase

--- Historia_Romance/src/narrador/DescripcionTiempo.java
+++ Historia_Romance/src/narrador/DescripcionTiempo.java
@@ -3,7 +3,9 @@ package narrador;
 public class DescripcionTiempo {
 
 	public static void main(String[] args) {
-		System.out.println("Descripcion del tiempo: Ambientado en la España rural del siglo XV. El narrador se trata de un personaje de la historia, asi que el tiempo es en preterito para comentar la historia");
+		System.out.println("Descripcion del tiempo: Ambientado en la España rural del siglo XV. "
+				+ "El narrador se trata de un personaje de la historia, asi que el tiempo es en "
+				+ "preterito para comentar la historia");
 
 	}
 
---
+++ Historia_Romance/src/umbra/PersonajeUmbra.java
@@ -0,0 +1,10 @@
+package umbra;
+
+public class PersonajeUmbra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
