7a370473451d39db932ac0fc20311ff7eb6c87f7
Kelly Vasquez <kellyvasquez1921@gmail.com>
2024-11-27 17:20:44 +0100

    Cambios en los textos

--- Historia_Romance/src/narrador/Lugar.java
+++ Historia_Romance/src/narrador/Lugar.java
@@ -3,7 +3,8 @@ package narrador;
 public class Lugar {
 
 	public static void main(String[] args) {
-		System.out.println("Descripcion del lugar: Un pais llamado \"Umbra\", donde la gran mayoria de la poblacion esta en la miseria. ");
+		System.out.println("Descripcion del lugar: Un pais llamado \"Umbra\", "
+				+ "donde la gran mayoria de la poblacion esta en la miseria. ");
 
 	}
 
--- Historia_Romance/src/narrador/Sociedad.java
+++ Historia_Romance/src/narrador/Sociedad.java
@@ -3,7 +3,9 @@ package narrador;
 public class Sociedad {
 
 	public static void main(String[] args) {
-		System.out.println("Descripcion de la sociedad: Un 95% constituido por el pueblo llano, con un porcentaje minusculo de clases altas constituida por los politicos o gente adinerada con mucho poder.");
+		System.out.println("Descripcion de la sociedad: Un 95% constituido por el pueblo llano, "
+				+ "con un porcentaje minusculo de clases altas constituida por los politicos "
+				+ "o gente adinerada con mucho poder.");
 
 	}
 
