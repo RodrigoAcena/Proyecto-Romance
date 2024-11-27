105877176b4715897d24b4b1a8ceb4eeda8b59c4
RodrigoAcena <24.damt.rodrigoacena@fundacionmontessori.com>
2024-11-27 17:46:33 +0100

    Cambios del Nomada y Narrador

--- Historia_Romance/src/narrador/DescripcionTiempo.java
+++ Historia_Romance/src/narrador/DescripcionTiempo.java
@@ -4,7 +4,7 @@ public class DescripcionTiempo {
 
 	public static void main(String[] args) {
 		System.out.println("Descripcion del tiempo: Ambientado en la España rural del siglo XV. "
-				+ "El narrador se trata de un personaje de la historia, asi que el tiempo es en "
+				+ "\nEl narrador se trata de un personaje de la historia, asi que el tiempo es en "
 				+ "preterito para comentar la historia");
 
 	}
--- Historia_Romance/src/nomada/DescripcionNomada.java
+++ Historia_Romance/src/nomada/DescripcionNomada.java
@@ -4,10 +4,11 @@ public class DescripcionNomada {
 
 	public static void main(String[] args) {
 		System.out.println("Descripcion Fisica: Hombre flaco de mediana edad con piel sucia, pelo rizado y moreno. "
-				+ "Sus ojos son color café y tiene algo de napia, uñas largas y los dientes descolocados. \r\n"
+				+ "\nSus ojos son color café y tiene algo de napia, uñas largas y los dientes descolocados.\r\n"
 				+ "\r\n"
 				+ "Historia: Se trata de una persona misteriosa que se dedica a ayudar al pueblo de Umbra como un buen samaritano. "
-				+ "Suele de un lugar a otro en busca de personas a las que ayudar pero normalmente frecuenta el poblado de Cira\r\n"
+				+ "\nNo evoluciona mucho a lo largo de la trama, simplemente se encuentra alli como narrador testigo y apoyo a los protagonistas."
+				+ "\nSuele de un lugar a otro en busca de personas a las que ayudar pero normalmente frecuenta el poblado de Cira\r\n"
 				+ "\r\n"
 				+ "Frase Tipica: Joven pastora (para referirse a Cira)");
 
